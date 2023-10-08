995e057aa3eea2479dca278db14a7c3212b51e9a
Tahsin Başar Paksoy <tahsin.basar.paksoy.019@gmail.com>
2023-10-08 23:32:30 +0300

    8.10.2023
    - Delta text added to Waterfall chart.
    - Value text added to Pie chart.

--- PieChart.cs
+++ PieChart.cs
@@ -12,23 +12,30 @@ public partial class PieChart : Chart
         foreach (float f in data)
             total += f;
         float t = 0f;
-        Stack<(float, float)> angles = new Stack<(float, float)>();
+        List<(float, float)> angles = new List<(float, float)>();
         for (int i = 0; i < data.Count; i++)
         {
             float first = t / total;
             t += data[i];
             float second = t / total;
-            angles.Push((first, second));
+            angles.Add((first, second));
         }
         Vector2 s = GetRect().Size;
         Vector2 center = s / 2f;
         float size = Mathf.Min(s.X, s.Y) / 2;
-        int index = 0;
-        while (angles.Count > 0)
+        /*while (angles.Count > 0)
         {
             (float, float) temp = angles.Pop();
-            index = (index + 1) % colors.Length;
             DrawArcPoly(center, size, temp.Item1 * 360f, temp.Item2 * 360f, colors[index % colors.Length]);
+            index = (index + 1) % colors.Length;
+        }*/
+        for (int i = 0; i < data.Count; i++)
+        {
+            DrawArcPoly(center, size, angles[i].Item1 * 360f, angles[i].Item2 * 360f, colors[i % colors.Length]);
+        }
+        for (int i = 0; i < data.Count; i++)
+        {
+            DrawValue(center, data[i].ToString(), angles[i].Item1 * 360f, angles[i].Item2 * 360, size / 2f);
         }
     }
     private void DrawArcPoly(Vector2 center, float radius, float angleFrom, float angleTo, Color color)
@@ -37,13 +44,19 @@ public partial class PieChart : Chart
         var pointsArc = new Vector2[nbPoints + 2];
         pointsArc[0] = center;
         var colors = new Color[] { color };
-
         for (int i = 0; i <= nbPoints; i++)
         {
             float anglePoint = Mathf.DegToRad(angleFrom + i * (angleTo - angleFrom) / nbPoints - 90);
             pointsArc[i + 1] = center + new Vector2(Mathf.Cos(anglePoint), Mathf.Sin(anglePoint)) * radius;
         }
-
         DrawPolygon(pointsArc, colors);
+
+
+    }
+    private void DrawValue(Vector2 center, string text, float fromAngle, float toAngle, float distance)
+    {
+        float middleAngle = (toAngle + fromAngle) / 2f;
+        GD.Print(middleAngle);
+        DrawString(GetThemeDefaultFont(), center + new Vector2(Mathf.Cos(Mathf.DegToRad(middleAngle - 90f)), Mathf.Sin(Mathf.DegToRad(middleAngle - 90f))) * distance, text, alignment: HorizontalAlignment.Center);
     }
 }
--- WaterfallChart.cs
+++ WaterfallChart.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+
 [GlobalClass]
 [Tool]
 public partial class WaterfallChart : Chart
@@ -7,10 +8,13 @@ public partial class WaterfallChart : Chart
     [Export]
     public Color startAndEndColor, colorOnDecrease, colorOnIncrease;
     [Export]
-    public bool writeDelta;
-    public int deltaFontSize;
+    public bool writeValues;
+    [Export]
+    public int fontSize;
+
     public override void _Draw()
     {
+
         if (data.Count < 3) return;
         float min = float.MaxValue, max = float.MinValue;
         foreach (float f in data)
@@ -25,32 +29,33 @@ public partial class WaterfallChart : Chart
         if (xPerColumn == 0f || yPerValue == 0f) return;
         if (Mathf.Sign(min) >= 0f && Mathf.Sign(max) >= 0f)
         {
-            DrawColumn(0, new Vector2(0f, size.Y), new Vector2(xPerColumn, -Mathf.Abs(yPerValue * data[0])));
+            DrawColumn(0, new Vector2(0f, size.Y), new Vector2(xPerColumn, -Mathf.Abs(yPerValue * data[0])), writeValues ? data[0].ToString() : null);
             float total = data[0] * yPerValue;
             for (int i = 1; i < data.Count; i++)
             {
                 float diffrence = data[i - 1] - data[i];
-                DrawColumn(i, new Vector2(xPerColumn * i, size.Y - total), new Vector2(xPerColumn, diffrence * yPerValue), diffrence < 0 ? colorOnIncrease : colorOnDecrease);
+                DrawColumn(i, new Vector2(xPerColumn * i, size.Y - total), new Vector2(xPerColumn, diffrence * yPerValue), diffrence < 0 ? colorOnIncrease : colorOnDecrease, writeValues ? (-diffrence).ToString() : null);
                 total -= yPerValue * diffrence;
             }
-            DrawColumn(data.Count - 1, size - new Vector2(xPerColumn, 0f), new Vector2(xPerColumn, -Mathf.Abs(yPerValue * data[^1])));
+            DrawColumn(data.Count - 1, size - new Vector2(xPerColumn, 0f), new Vector2(xPerColumn, -Mathf.Abs(yPerValue * data[^1])), writeValues ? data[^1].ToString() : null);
         }
         else if (Mathf.Sign(min) >= 0f ^ Mathf.Sign(max) >= 0f)
         {
-            DrawColumn(0, new Vector2(0f, zeroPoint - data[0] * yPerValue), new Vector2(xPerColumn, yPerValue * data[0]));
+            DrawColumn(0, new Vector2(0f, zeroPoint - data[0] * yPerValue), new Vector2(xPerColumn, yPerValue * data[0]), writeValues ? data[0].ToString() : null);
             float total = zeroPoint - data[0] * yPerValue;
             for (int i = 1; i < data.Count; i++)
             {
                 float diffrence = data[i - 1] - data[i];
-                DrawColumn(i, new Vector2(xPerColumn * i, total), new Vector2(xPerColumn, diffrence * yPerValue), diffrence < 0 ? colorOnIncrease : colorOnDecrease);
+                DrawColumn(i, new Vector2(xPerColumn * i, total), new Vector2(xPerColumn, diffrence * yPerValue), diffrence < 0 ? colorOnIncrease : colorOnDecrease, writeValues ? (-diffrence).ToString() : null);
                 total += yPerValue * diffrence;
             }
-            DrawColumn(data.Count - 1, new Vector2(size.X - xPerColumn, zeroPoint - yPerValue * data[^1]), new Vector2(xPerColumn, yPerValue * data[^1]));
+            DrawColumn(data.Count - 1, new Vector2(size.X - xPerColumn, zeroPoint - yPerValue * data[^1]), new Vector2(xPerColumn, yPerValue * data[^1]), writeValues ? data[^1].ToString() : null);
         }
         if (min < 0f) DrawLine(new Vector2(0f, zeroPoint), new Vector2(size.X, zeroPoint), new Color(1f, 0f, 0f), width: 2f);
 
+
     }
-    private void DrawColumn(int index, Vector2 begin, Vector2 offset)
+    private void DrawColumn(int index, Vector2 begin, Vector2 offset, string text = null)
     {
         float f = data[index];
         Vector2[] points = new Vector2[4];
@@ -59,8 +64,10 @@ public partial class WaterfallChart : Chart
         points[2] = begin + offset;
         points[3] = begin + offset with { X = 0 };
         DrawPolygon(points, new Color[] { startAndEndColor, startAndEndColor, startAndEndColor, startAndEndColor });
+        if (text == null) return;
+        DrawString(GetThemeDefaultFont(), (points[0] + points[3]) / 2f, text, alignment: HorizontalAlignment.Fill, fontSize: fontSize);
     }
-    private void DrawColumn(int index, Vector2 begin, Vector2 offset, Color color)
+    private void DrawColumn(int index, Vector2 begin, Vector2 offset, Color color, string text = null)
     {
         float f = data[index];
         Vector2[] points = new Vector2[4];
@@ -69,5 +76,7 @@ public partial class WaterfallChart : Chart
         points[2] = begin + offset;
         points[3] = begin + offset with { X = 0 };
         DrawPolygon(points, new Color[] { color, color, color, color });
+        if (text == null) return;
+        DrawString(GetThemeDefaultFont(), (points[0] + points[3]) / 2f, text, alignment: HorizontalAlignment.Fill, fontSize: fontSize);
     }
 }
